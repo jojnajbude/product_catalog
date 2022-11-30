3ddd398e0ba73b6b44f57b6cd47fd405746839a9
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-11-30 16:27:36 +0200

    eslint config

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <header className="grid grid-tablet grid-desktop">
+      <header className="grid grid-mobile grid-tablet grid-desktop">
         <div className="grid-mobile-1-2 grid-tablet-5-7">Test</div>
         <div className="grid-mobile-2-4 grid-tablet-10-11">HELLO MATES</div>
       </header>
