deb8e034442ed4093887f0b3ca028090072e21f2
Arsen Shpak <110231219+Arsen-Shpak@users.noreply.github.com>
2022-12-02 01:51:46 +0200

    Footer (#11)
    * footer
    
    * footer
    
    Co-authored-by: Dima Romanenko <79811705+iPanda177@users.noreply.github.com>

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,35 @@
+import logo from './images/Logo.svg';
+import vector from './images/Vector.svg';
+
+export const Footer: React.FC = () => {
+    return (
+      <footer className="footer">
+        <div className="footer__wrapper">
+          <div className="footer__logo">
+            <a href="*">
+              <img src={logo} alt="img" />
+            </a>
+          </div>
+          <nav className="footer__nav">
+            <a href="*" className="footer__nav-link">
+              Github
+            </a>
+            <a href="*" className="footer__nav-link">
+              Contacts
+            </a>
+            <a href="*" className="footer__nav-link">
+              rights
+            </a>
+          </nav>
+          <div className="footer__back">
+            <label className="footer__back-article" htmlFor="back">
+              Back to top
+            </label>
+            <button className="footer__back-link" id="back">
+              <img className="footer__back-img" src={vector} alt="img" />
+            </button>
+          </div>
+        </div>
+      </footer>
+    ); 
+}
