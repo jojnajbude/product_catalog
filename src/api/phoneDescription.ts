7454bf9072bca28a54b582b231f859bf94b06565
Serhii Romaniuk <deepspark@gmail.com>
2022-12-02 11:18:26 +0200

    implement getPhones func

--- src/api/phoneDescription.ts
+++ src/api/phoneDescription.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import { PhoneDescr } from '../utils/types/PhoneDescription';
+import { Phone } from '../types/Phone';
 
 const BASE_URL = 'https://productcatalogapi-production.up.railway.app/phones/'
+
+export const getPhones = async () => {
+  return await axios.get<Phone[]>(BASE_URL)
+    .then((response) => response.data);
+}
 
 export const getPhoneDescription = async (id :string) => {
   return await axios.get<PhoneDescr>(BASE_URL+id)
